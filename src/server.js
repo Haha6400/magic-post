01db8a536bab80ef8aca67544603c73fe9f19ccf
Nguyễn Đăng Dương <21020757@vnu.edu.vn>
2023-11-07 02:38:37 +0700

    chore: rename app.js to server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,11 @@
 var express = require('express'); // requre the express framework
 var app = express();
 var fs = require('fs'); //require file system object
+var route = require('./routes')
 
+//Parse data stream from client
+app.use(express.json())
+route(app)
 
 // Create a server to listen at port 3000
 var server = app.listen(3000, function(){
