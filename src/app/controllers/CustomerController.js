0a074157acc510073111f56415cfd67fc09602f5
Nguyễn Đăng Dương <21020757@vnu.edu.vn>
2023-11-07 02:32:34 +0700

    feat(controllers): create controllers and  customer controller

---
+++ src/app/controllers/CustomerController.js
@@ -0,0 +1,35 @@
+class CustomerController {
+    //@desc Get all orders
+    //@route GET /home/order
+    getAllOrders(req, res) {
+        res.status(200).json({ message: 'GET ALL ORDERS' })
+    }
+
+    //@desc Post order
+    //@route GET /home/order
+    createOrder(req, res) {
+        console.log(req.body)
+        res.status(201).json({ message: 'POST ORDER' })
+    }
+
+    //@desc Get order
+    //@route GET /home/order/:id
+    getOrder(req, res) {
+        res.status(200).json({ message: `GET ORDER FOR ID ${req.params.id}` })
+    }
+
+
+    //@desc Update order
+    //@route PUT /home/order/:id
+    updateOrder(req, res) {
+        res.status(201).json({ message: `UPDATE ORDER FOR ID ${req.params.id}` })
+    }
+
+    //@desc Delete order
+    //@route DELETE /home/order/:id
+    deleteOrder(req, res) {
+        res.status(200).json({ message: `DELETE ORDER FOR ID ${req.params.id}` })
+    }
+}
+
+module.exports = new CustomerController;
