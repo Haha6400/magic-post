59b4eac4bf4354114634a4e22319337ce8c3edc1
Haha06 <21020310@vnu.edu.vn>
2023-11-06 10:19:19 +0700

    initialize project

---
+++ app.js
@@ -0,0 +1,12 @@
+//REST API demo in Node.js
+var express = require('express'); // requre the express framework
+var app = express();
+var fs = require('fs'); //require file system object
+
+
+// Create a server to listen at port 3000
+var server = app.listen(3000, function(){
+    var host = server.address().address
+    var port = server.address().port
+    console.log("REST API listening at http://%s:%s", host, port)
+})
