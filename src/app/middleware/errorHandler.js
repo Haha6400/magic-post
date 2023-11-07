62d6343f7ccc503a94441f8f7cd4b503ddeda907
Haha6400 <100473336+Haha6400@users.noreply.github.com>
2023-11-07 18:57:26 +0700

    Update errorHandler.js

--- src/app/middleware/errorHandler.js
+++ src/app/middleware/errorHandler.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode ? res.statusCode : 500
+    const statusCode = res.statusCode ? res.statusCode : 500;
     res.json({ message: err.message, stackTrace: err.stack })
-}
+};
 
-module.exports = errorHandler
+module.exports = errorHandler;
