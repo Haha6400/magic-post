a30a2a676d074eca85b2f9314c7d75c7ceadaabb
Haha06 <21020310@vnu.edu.vn>
2023-11-07 23:59:25 +0700

    add order files

---
+++ src/config/rolesList.js
@@ -0,0 +1,8 @@
+const ROLES_LIST = {
+    "hubStaff": 1000,
+    "warehouseStaff": 2000, 
+    "hubManager": 3000, 
+    "warehouseManager": 4000, 
+    "supervisor": 5000
+}
+module.exports = ROLES_LIST
---
+++ src/server.js
@@ -0,0 +1,23 @@
+//REST API demo in Node.js
+const express = require('express'); // requre the express framework
+const app = express();
+const errorHandler = require("./app/middleware/errorHandler"); 
+var fs = require('fs'); //require file system object
+var route = require('./routes')
+const connectDB = require('./config/dbConnection');
+const dotenv = require("dotenv").config();
+
+connectDB();
+// const staff = require('./models/staff'); //import class staff
+
+app.use(express.json());
+app.use(errorHandler);
+app.use("/api/accounts", require("./routes/staffRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+
+// create a server to listen at port 3000
+var server = app.listen(3000, function(){
+    var host = server.address().address
+    var port = server.address().port
+    console.log("REST API listening at http://%s:%s", host, port)
+})
