5eed9c2eb8ac362c587f7cd9a4694eb9edcc6789
Haha06 <21020310@vnu.edu.vn>
2023-11-07 09:58:07 +0700

    feat: update app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-//REST API demo in Node.js
+
 var express = require('express'); // requre the express framework
 var app = express();
 var fs = require('fs'); //require file system object
 
+const staff = require('./models/staff'); //import class staff
 
-// Create a server to listen at port 3000
+app.use(express.json());
+
+// create a server to listen at port 3000
 var server = app.listen(3000, function(){
     var host = server.address().address
     var port = server.address().port
