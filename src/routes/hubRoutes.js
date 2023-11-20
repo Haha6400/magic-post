b3635aedd35a6afe601903dd556c503b60dc2d49
Haha06 <21020310@vnu.edu.vn>
2023-11-20 17:44:57 +0700

    replace branch id

--- src/routes/hubRoutes.js
+++ src/routes/hubRoutes.js
@@ -9,27 +9,27 @@ const { allHubReceive_Manager, allHubReceive_Supervisor, allHubSend_Manager, all
 /*-------------------------------------------------------ALL ORDERS-------------------------------------------------------*/
 //@access hubManager
 router.get('/receive/all', staffAuth, roleCheck(["hubManager"]), allHubReceive_Manager);
-router.get('/receive/all/:branch_id', staffAuth, roleCheck(["hubManager"]), allHubReceiveByWH_Manager);
+router.get('/receive/all/wh', staffAuth, roleCheck(["hubManager"]), allHubReceiveByWH_Manager);
 router.get('/send/all', staffAuth, roleCheck(["hubManager"]), allHubSend_Manager);
-router.get('/send/all/:branch_id', staffAuth, roleCheck(["hubManager"]), allHubSendByWH_Manager);
+router.get('/send/all/wh', staffAuth, roleCheck(["hubManager"]), allHubSendByWH_Manager);
 
 //@access supervisor
-router.get('/receive/s/all/:hub_id', staffAuth, roleCheck("supervisor"), allHubReceive_Supervisor);
-router.get('/receive/s/all/:hub_id/:warehouse_id', staffAuth, roleCheck(["supervisor"]), allHubReceivByWH_Supervisor);
-router.get('/send/s/all/:hub_id', staffAuth, roleCheck(["supervisor"]), allHubSend_Supervisor);
-router.get('/send/s/all/:hub_id/:warehouse_id', staffAuth, roleCheck(["supervisor"]), allHubSendByWH_Supervisor);
+router.get('/receive/s/all', staffAuth, roleCheck("supervisor"), allHubReceive_Supervisor);
+router.get('/receive/s/all/wh', staffAuth, roleCheck(["supervisor"]), allHubReceivByWH_Supervisor);
+router.get('/send/s/all', staffAuth, roleCheck(["supervisor"]), allHubSend_Supervisor);
+router.get('/send/s/all/wh', staffAuth, roleCheck(["supervisor"]), allHubSendByWH_Supervisor);
 
 /*-------------------------------------------------------ORDERS ARE AVAILABLE AT HUB-------------------------------------------------------*/
 //@access hubManager
 router.get('/receive/available', staffAuth, roleCheck(["hubManager"]), availableHubReceive_Manager);
-router.get('/receive/available/:branch_id', staffAuth, roleCheck(["hubManager"]), availableHubReceiveByWH_Manager);
+router.get('/receive/available/wh', staffAuth, roleCheck(["hubManager"]), availableHubReceiveByWH_Manager);
 router.get('/send/available', staffAuth, roleCheck(["hubManager"]), availableHubSend_Manager);
-router.get('/send/available/:branch_id', staffAuth, roleCheck(["hubManager"]), availableHubSendByWH_Manager);
+router.get('/send/available/wh', staffAuth, roleCheck(["hubManager"]), availableHubSendByWH_Manager);
 
 //@access supervisor
-router.get('/receive/s/available/:hub_id', staffAuth, roleCheck(["supervisor"]), availableHubReceive_Supervisor);
-router.get('/receive/s/available/:hub_id/:warehouse_id', staffAuth, roleCheck(["supervisor"]), availableHubReceivByWH_Supervisor);
-router.get('/send/s/available/:hub_id', staffAuth, roleCheck(["supervisor"]), availableHubSend_Supervisor);
-router.get('/send/s/available/:hub_id/:warehouse_id', staffAuth, roleCheck(["supervisor"]), availableHubSendByWH_Supervisor);
+router.get('/receive/s/available', staffAuth, roleCheck(["supervisor"]), availableHubReceive_Supervisor);
+router.get('/receive/s/available/wh', staffAuth, roleCheck(["supervisor"]), availableHubReceivByWH_Supervisor);
+router.get('/send/s/available', staffAuth, roleCheck(["supervisor"]), availableHubSend_Supervisor);
+router.get('/send/s/available/wh', staffAuth, roleCheck(["supervisor"]), availableHubSendByWH_Supervisor);
 
 module.exports = router; 
