f7eecf9362f5206b9f69dded80005a7e5e5fc11b
Tan Duc <ductan10092003@gmail.com>
2024-02-07 21:40:22 +0700

    adding dist

---
+++ frontend/dist/assets/AboutView-4718bf48.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as _}from"./index-adea1279.js";const c={};function t(n,s){return o(),_("p",null,"More about us - Coming soon")}const r=e(c,[["render",t],["__scopeId","data-v-c89b3281"]]);export{r as default};
